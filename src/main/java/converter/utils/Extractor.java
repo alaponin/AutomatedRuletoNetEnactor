9fc010db1270b32b7da70e738a12d9d012dc193b
Arne Lapõnin <arne.laponin@gmail.com>
2017-02-01 18:13:52 +0100

    Removed unnecessary imports

--- src/main/java/converter/utils/Extractor.java
+++ src/main/java/converter/utils/Extractor.java
@@ -1,34 +1,15 @@
 package converter.utils;
 
-import org.processmining.framework.connections.Connection;
-import org.processmining.framework.connections.ConnectionCannotBeObtained;
-import org.processmining.framework.connections.ConnectionManager;
-import org.processmining.framework.plugin.*;
-import org.processmining.framework.plugin.events.Logger;
-import org.processmining.framework.plugin.events.PluginLifeCycleEventListener;
-import org.processmining.framework.plugin.events.ProgressEventListener;
-import org.processmining.framework.plugin.impl.AbstractPluginContext;
-import org.processmining.framework.plugin.impl.FieldSetException;
-import org.processmining.framework.providedobjects.ProvidedObjectManager;
-import org.processmining.framework.util.Pair;
 import org.processmining.models.connections.GraphLayoutConnection;
-import org.processmining.models.graphbased.directed.petrinet.Petrinet;
 import org.processmining.models.graphbased.directed.petrinet.PetrinetGraph;
 import org.processmining.models.graphbased.directed.petrinet.impl.PetrinetFactory;
-import org.processmining.models.graphbased.directed.petrinet.impl.PetrinetImpl;
 import org.processmining.models.semantics.petrinet.Marking;
 import org.processmining.plugins.pnml.Pnml;
-import org.processmining.plugins.pnml.exporting.PnmlExportNet;
-import org.processmining.plugins.pnml.exporting.PnmlExportNetToPNML;
 import org.processmining.plugins.pnml.importing.PnmlImportUtils;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStream;
-import java.util.Collection;
-import java.util.List;
-import java.util.concurrent.Executor;
 
 /**
  * Created by arnelaponin on 02/09/16.
