a428c14c25bb56ce0ab8231a22873df56fcc6667
Arne Lapõnin <arne.laponin@gmail.com>
2017-02-06 15:34:30 +0100

    helpful comments + repair when loops are in the net

--- src/main/java/converter/utils/Repairer.java
+++ src/main/java/converter/utils/Repairer.java
@@ -1,15 +1,14 @@
 package converter.utils;
 
+import org.processmining.models.graphbased.directed.petrinet.Petrinet;
 import org.processmining.models.graphbased.directed.petrinet.PetrinetEdge;
 import org.processmining.models.graphbased.directed.petrinet.PetrinetGraph;
 import org.processmining.models.graphbased.directed.petrinet.PetrinetNode;
 import org.processmining.models.graphbased.directed.petrinet.elements.Place;
 import org.processmining.models.graphbased.directed.petrinet.elements.Transition;
+import org.processmining.models.graphbased.directed.petrinet.impl.PetrinetFactory;
 
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created by arnelaponin on 03/11/2016.
@@ -35,6 +34,18 @@ public class Repairer {
             }
             System.out.println("Transition after source: " + transitionsAfterSource);
 
+            //Necessary because of the loops
+            Collection<PetrinetEdge<? extends PetrinetNode, ? extends PetrinetNode>> inEdgesToNextPlace = net.getInEdges(nextPlaceAfterSource);
+            List<Transition> transitionsThatHaveToBeConnectedToNewPlace = new ArrayList<>();
+            if (inEdgesToNextPlace.size() > 1) {
+                for (PetrinetEdge edge : inEdgesToNextPlace) {
+                    Transition t = (Transition) edge.getSource();
+                    if (!t.equals(source)) {
+                        transitionsThatHaveToBeConnectedToNewPlace.add(t);
+                    }
+                }
+            }
+
             String label = nextPlaceAfterSource.getLabel();
             net.removePlace(nextPlaceAfterSource);
             System.out.println("Removing place: " + label);
@@ -44,13 +55,13 @@ public class Repairer {
             if (transitionsAfterSource.size() == 1) {
                 Transition t = transitionsAfterSource.iterator().next();
                 if (!areTransitionsConnected(net, target, t)) {
-                    addNewPlace(net, target, transitionsAfterSource);
+                    addNewPlace(net, target, transitionsAfterSource, transitionsThatHaveToBeConnectedToNewPlace);
                 }
             } else {
-                addNewPlace(net, target, transitionsAfterSource);
+                addNewPlace(net, target, transitionsAfterSource, transitionsThatHaveToBeConnectedToNewPlace);
             }
         }
-        PetrinetUtils.exportPetriNetToPNML("test_repair_OF_NEW_ALGORITHM_DEUX.pnml", net);
+
         return net;
     }
 
@@ -69,50 +80,103 @@ public class Repairer {
         return false;
     }
 
-    private static void addNewPlace(PetrinetGraph net, Transition target, Set<Transition> transitionsAfterSource) {
+    private static void addNewPlace(PetrinetGraph net, Transition target, Set<Transition> transitionsAfterSource, List<Transition> transitionsThatHaveToBeConnectedToNewPlace) {
         Place place1 = net.addPlace("p");
         net.addArc(target, place1);
         for (Transition t : transitionsAfterSource) {
             net.addArc(place1, t);
         }
+        if (!transitionsThatHaveToBeConnectedToNewPlace.isEmpty()) {
+            for (Transition t : transitionsThatHaveToBeConnectedToNewPlace) {
+                net.addArc(t, place1);
+            }
+        }
     }
 
-    public static PetrinetGraph repair(PetrinetGraph net, Place place, Place placeToRemove) throws Exception {
+    private static Place getPlaceFromCloneNet(Petrinet net, Place p) {
+        for (Place place : net.getPlaces()) {
+            if (place.getLabel().equalsIgnoreCase(p.getLabel())) {
+                return place;
+            }
+        }
+        return null;
+    }
 
-        String fileName = "test_repair_" + place.getLabel() + ".pnml";
+    public static PetrinetGraph addSyncPointInsteadOfFlattening(PetrinetGraph originalNet, Place troubledPlace, Place problematicPlace) {
+
+        Collection<PetrinetEdge<? extends PetrinetNode, ? extends PetrinetNode>> edgesInPlace = originalNet.getInEdges(troubledPlace);
+        Collection<PetrinetEdge<? extends PetrinetNode, ? extends PetrinetNode>> edgesOutPlace = originalNet.getOutEdges(troubledPlace);
+        Transition transitionConnectedPlace = null;
+        if (edgesInPlace.size() == 1) {
+            for (PetrinetEdge edge : edgesInPlace) {
+                transitionConnectedPlace = (Transition) edge.getSource();
+            }
+        } else if (edgesInPlace.size() > 1) {
+            if (edgesOutPlace.size() == 1) {
+                for (PetrinetEdge edge : edgesOutPlace) {
+                    transitionConnectedPlace = (Transition) edge.getTarget();
+                }
+            }
+        }
+        Transition dangling = null;
+        if (problematicPlace != null) {
+            dangling = precedingTransOfToRemoveHasOneOutgoingArc(originalNet, problematicPlace);
+        }
+
+        Map<Transition, Transition> netRepairPair = new HashMap<>();
+        PetrinetGraph petrinetGraph = null;
+        if (transitionConnectedPlace != null && dangling != null) {
+            netRepairPair.put(transitionConnectedPlace, dangling);
+            System.out.println("NET REPAIR PAIR: " + netRepairPair);
+            petrinetGraph = putSyncPoints(originalNet, netRepairPair);
+            String syncFileName = "test_sync_in_" + troubledPlace + "-" + problematicPlace + ".pnml";
+            PetrinetUtils.exportPetriNetToPNML(syncFileName, petrinetGraph);
+        }
+        return petrinetGraph;
+    }
+
+    public static PetrinetGraph repair(PetrinetGraph net, Place troubledPlace, Place problematicPlace) throws Exception {
+        System.out.println("Place: " + troubledPlace);
+        System.out.println("Problematic place to remove: " + problematicPlace);
+
+        String fileName = "test_repair_" + troubledPlace.getLabel() + "_" + problematicPlace + ".pnml";
         System.out.println("File name: " + fileName);
 
-        Collection<PetrinetEdge<? extends PetrinetNode, ? extends PetrinetNode>> edges = net.getOutEdges(place);
+        Collection<PetrinetEdge<? extends PetrinetNode, ? extends PetrinetNode>> edges = net.getOutEdges(troubledPlace);
 
+        Transition dangling = precedingTransOfToRemoveHasOneOutgoingArc(net, problematicPlace);
+
+        System.out.println("Dangling: " + dangling);
 
         for (PetrinetEdge edge : edges) {
             PetrinetNode source = (PetrinetNode) edge.getSource();
             PetrinetNode target = (PetrinetNode) edge.getTarget();
+            System.out.println("Removing arc between: " + source + " and " + target);
             net.removeArc(source, target);
         }
 
-        Collection<PetrinetEdge<? extends PetrinetNode, ? extends PetrinetNode>> outEdges = net.getOutEdges(placeToRemove);
+        Collection<PetrinetEdge<? extends PetrinetNode, ? extends PetrinetNode>> outEdges = net.getOutEdges(problematicPlace);
 
-        Transition dangling = precedingTransOfToRemoveHasOneOutgoingArc(net, placeToRemove);
+
 
         if (dangling != null) {
-            Collection<PetrinetEdge<? extends PetrinetNode, ? extends PetrinetNode>> inEdgesToPlace = net.getInEdges(place);
+            Collection<PetrinetEdge<? extends PetrinetNode, ? extends PetrinetNode>> inEdgesToPlace = net.getInEdges(troubledPlace);
             for (PetrinetEdge edgeIn : inEdgesToPlace) {
                 Transition tBeforePlace = (Transition) edgeIn.getSource();
-                String placeName = placeToRemove.getLabel();
-                net.removePlace(placeToRemove);
+                String placeName = problematicPlace.getLabel();
+                net.removePlace(problematicPlace);
                 Place newlyAddedPlace = net.addPlace(placeName);
                 net.addArc(dangling, newlyAddedPlace);
-                System.out.println("ADDING arc between: " + dangling + " and " + newlyAddedPlace);
+                System.out.println("1. ADDING arc between: " + dangling + " and " + newlyAddedPlace);
                 net.addArc(newlyAddedPlace, tBeforePlace);
-                System.out.println("ADDING arc between: " + newlyAddedPlace + " and " + tBeforePlace);
+                System.out.println("2. ADDING arc between: " + newlyAddedPlace + " and " + tBeforePlace);
             }
 
             for (PetrinetEdge edgeOut : outEdges) {
                 Transition transition = (Transition) edgeOut.getTarget();
                 System.out.println("Will be adding to this transition: " + transition);
-                net.addArc(place,transition);
-                System.out.println("ADDING arc between: " + place + " and " + transition);
+                net.addArc(troubledPlace,transition);
+                System.out.println("3. ADDING arc between: " + troubledPlace + " and " + transition);
             }
 
         } else {
@@ -120,11 +184,11 @@ public class Repairer {
 
                 Transition transition = (Transition) edgeOut.getTarget();
 
-                System.out.println("ADDING arc between: " + place + " and " + transition);
-                net.addArc(place, transition);
+                System.out.println("4. ADDING arc between: " + troubledPlace + " and " + transition);
+                net.addArc(troubledPlace, transition);
             }
-            System.out.println("Removing: " + placeToRemove);
-            net.removePlace(placeToRemove);
+            System.out.println("Removing: " + problematicPlace);
+            net.removePlace(problematicPlace);
         }
 
         PetrinetUtils.exportPetriNetToPNML(fileName, net);
